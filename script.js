f5f38216cf8dd08223a397b961bbaea5ea9fcaf2
Fatma Oymak <bedizma@hotmail.com>
2023-01-15 23:36:30 +0000

    final answer

---
+++ script.js
@@ -0,0 +1,9 @@
+const currentBook = "War and Peace";
+let myNextBook = "The Three Muskeeters";
+
+console.log("Finished" + currentBook + ". I'll now start reading " + myNextBook);
+
+currentBook = myNextBook;
+myNextBook = "All Quiet on the Western Front";
+
+console.log("Now currently reading " + currentBook + ". After I'm Finished with this one, I'll be reading " + myNextBook);
